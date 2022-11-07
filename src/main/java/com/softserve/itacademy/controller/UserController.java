aca3348a306ad9a3394ddf98f6529b6e753e683e
4Vitalii5 <pavlykvitalii@gmail.com>
2022-11-08 01:37:35 +0200

    UserController

--- src/main/java/com/softserve/itacademy/controller/UserController.java
+++ src/main/java/com/softserve/itacademy/controller/UserController.java
@@ -1,99 +1,112 @@
 package com.softserve.itacademy.controller;
 
+import com.softserve.itacademy.dto.UserRequestDto;
+import com.softserve.itacademy.dto.UserResponseDto;
 import com.softserve.itacademy.model.User;
 import com.softserve.itacademy.service.RoleService;
 import com.softserve.itacademy.service.UserService;
+import jdk.dynalink.Operation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.security.Principal;
 import java.util.List;
+import java.util.stream.Collectors;
 
-@Controller
-@RequestMapping("/users")
+@RestController
+@Slf4j
+@RequestMapping("/api/users")
 public class UserController {
 
     private final UserService userService;
     private final RoleService roleService;
+
 
     public UserController(UserService userService, RoleService roleService) {
         this.userService = userService;
         this.roleService = roleService;
     }
 
-    @GetMapping("/create")
-    public String create(Model model) {
-        model.addAttribute("user", new User());
-        return "create-user";
-    }
-
-    @PostMapping("/create")
-    public String create(@Validated @ModelAttribute("user") User user, BindingResult result) {
-        if (result.hasErrors()) {
-            return "create-user";
-        }
-        user.setPassword(user.getPassword());
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public ResponseEntity<?> create(@RequestBody UserRequestDto userRequestDto) {
+        log.info("[POST] Request to create user");
+        User user = new User();
+        user.setFirstName(userRequestDto.getFirstName());
+        user.setLastName(userRequestDto.getLastName());
+        user.setEmail(userRequestDto.getEmail());
+        user.setPassword(userRequestDto.getPassword());
         user.setRole(roleService.readById(2));
-        User newUser = userService.create(user);
-        return "redirect:/todos/all/users/" + newUser.getId();
+        userService.create(user);
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(user.getId())
+                .toUri();
+        return ResponseEntity.created(location).body(new UserResponseDto(user));
     }
 
-    @GetMapping("/{id}/read")
-    @PreAuthorize("hasAuthority('ADMIN') or authentication.principal.id == #id")
-    public String read(@PathVariable long id, Model model) {
-        User user = userService.readById(id);
-        model.addAttribute("user", user);
-        return "user-info";
+    @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+//    @PreAuthorize("hasAuthority('ADMIN') or authentication.principal.id == #id")
+    public UserResponseDto read(@PathVariable long id) {
+        log.info("[GET] Request to read user");
+        return new UserResponseDto(userService.readById(id));
     }
 
-    @GetMapping("/{id}/update")
-    @PreAuthorize("hasAuthority('ADMIN') or authentication.principal.id == #id")
-    public String update(@PathVariable long id, Model model) {
-        User user = userService.readById(id);
-        model.addAttribute("user", user);
-        model.addAttribute("roles", roleService.getAll());
-        return "update-user";
-    }
-
-
-    @PostMapping("/{id}/update")
-    public String update(@PathVariable long id, Model model, @Validated @ModelAttribute("user") User user, @RequestParam("roleId") long roleId, BindingResult result) {
+    @PostMapping("/{id}/roles/{roleId}")
+//    @PreAuthorize("hasAuthority('ADMIN') or hasAuthority('USER') and authentication.principal.id == #id")
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseEntity<?> update(@PathVariable long id,
+                                    @PathVariable long roleId,
+                                    @RequestBody UserRequestDto userRequestDto,
+                                    Authentication authentication) {
         User oldUser = userService.readById(id);
-        if (result.hasErrors()) {
-            user.setRole(oldUser.getRole());
-            model.addAttribute("roles", roleService.getAll());
-            return "update-user";
+        oldUser.setFirstName(userRequestDto.getFirstName());
+        oldUser.setLastName(userRequestDto.getLastName());
+        oldUser.setLastName(userRequestDto.getLastName());
+        oldUser.setEmail(userRequestDto.getEmail());
+        oldUser.setPassword(userRequestDto.getPassword());
+        if (oldUser.getRole().getName().equals("USER")){
+            oldUser.setRole(oldUser.getRole());
+        }else{
+            oldUser.setRole(roleService.readById(roleId));
         }
-        user.setRole(roleService.readById(roleId));
-        userService.update(user);
-        return "redirect:/users/" + id + "/read";
-    }
+        userService.update(oldUser);
 
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id")
+                .buildAndExpand(oldUser.getId())
+                .toUri();
+        return ResponseEntity.created(location).body(new UserResponseDto(oldUser));
+    }
 
     @GetMapping("/{id}/delete")
-    @PreAuthorize("hasAuthority('ADMIN') or authentication.principal.id == #id")
-    public String delete(@PathVariable("id") long id, Principal principal) {
-        User securedUser = userService.readByEmail(principal.getName());
+//    @PreAuthorize("hasAuthority('ADMIN') or authentication.principal.id == #id")
+    public ResponseEntity delete(@PathVariable("id") long id, Authentication authentication) {
+        log.info("[GET] Request to delete user");
         userService.delete(id);
-        if(securedUser.getId() == id) {
-            return "redirect:/logout";
-        }
-        return "redirect:/users/all";
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
-    @GetMapping("/all")
-    public String getAll(Model model, Principal principal) {
-        User user = userService.readByEmail(principal.getName());
-        if(user.getRole().getName().equals("ADMIN")) {
-            model.addAttribute("users", userService.getAll());
-        } else {
-            model.addAttribute("users", List.of(user));
-        }
-        return "users-list";
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<UserResponseDto> getAll() {
+        log.info("[GET] Request to read all users");
+        return userService.getAll().stream()
+                .map(UserResponseDto:: new)
+                .collect(Collectors.toList());
     }
 }
