3487190d01ed0ec4d02eb63d736a9922dbbc1ed1
Bohdan Bazyl <pooplegenry@gmail.com>
2022-11-08 13:26:12 +0200

    Remove createdAt field form ToDoRequestDto

--- src/main/java/com/softserve/itacademy/dto/ToDoRequestDto.java
+++ src/main/java/com/softserve/itacademy/dto/ToDoRequestDto.java
@@ -1,9 +1,6 @@
 package com.softserve.itacademy.dto;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
-
-import java.time.LocalDateTime;
 
 @Data
 public class ToDoRequestDto {
